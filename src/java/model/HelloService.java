3877ac7edee31fe0704fbeb64568e5964b8c4849
Nick <Nick@NPARBSLAPTOP>
2016-08-29 20:03:51 -0500

    1

---
+++ src/java/model/HelloService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author nparbs
+ */
+public class HelloService {
+    public String createMsg(String name){
+       return "Hello " + name;
+    }
+}
